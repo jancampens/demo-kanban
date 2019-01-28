499f1f9fe41df8c5e6333967fe9a9ab5aea06934
Jan Campens <jan.campens@gmail.com>
2019-01-28 17:18:38 +0100

    Merge mock-applicants into mock-lists

--- src/app/mock/mock-lists.ts
+++ src/app/mock/mock-lists.ts
@@ -1,9 +1,36 @@
 import { List } from './../models/list';
 
 export const LISTS: List[] = [
-  { id: 524, name: 'Applied', order: 0, disabled: false },
-  { id: 752, name: 'Interview', order: 1, disabled: false },
-  { id: 415, name: 'Interview 2', order: 2, disabled: false },
-  { id: 525, name: 'Rejected', order: 3, disabled: false },
-  { id: 845, name: 'Added to group hired', order: 4, disabled: true }
+  {
+    id: 524, name: 'Applied', order: 0, disabled: false, color: '#F9F9F9', applicants: [
+      { name: 'Peter Dinklage', rating: 4.7, age: 49, gender: 'Male', city: 'Morristown', picture: '../../../assets/pictures/peter-dinklage.jpeg', list: 524 },
+      { name: 'Harry Lloyd', rating: 4.2, age: 35, gender: 'Male', city: 'London', picture: '../../../assets/pictures/harry-lloyd.jpeg', list: 524 },
+      { name: 'Sophie Turner', rating: 2.4, age: 22, gender: 'Female', city: 'Northampton', picture: '../../../assets/pictures/sophie-turner.jpeg', list: 524 }
+    ]
+  },
+  {
+    id: 752, name: 'Interview', order: 1, disabled: false, color: '#F9F9F9', applicants: [
+      { name: 'Isaac Hempstead Wright', rating: 3.8, age: 19, gender: 'Male', city: 'Surrey', picture: '../../../assets/pictures/isaac-hempstead-wright.jpeg', list: 752 }
+    ]
+  },
+  {
+    id: 415, name: 'Interview 2', order: 2, disabled: false, color: 'white', applicants: [
+      { name: 'Emilia Clarke', rating: 3.5, age: 32, gender: 'Female', city: 'London', picture: '../../../assets/pictures/emilia-clarke.jpeg', list: 415 },
+      { name: 'Kit Harington', rating: 5, age: 32, gender: 'Male', city: 'Surrey', picture: '../../../assets/pictures/kit-harington.jpeg', list: 415 },
+      { name: 'Lenay Heady', rating: 4.8, age: 45, gender: 'Female', city: 'Hamilton', picture: '../../../assets/pictures/lena-heady.jpeg', list: 415 }]
+  },
+  {
+    id: 525, name: 'Rejected', order: 3, disabled: false, color: '#F9F9F9', applicants: [
+      { name: 'Maisie Williams', rating: 4, age: 21, gender: 'Female', city: 'Bristol', picture: '../../../assets/pictures/maisie-williams.jpeg', list: 525 },
+      { name: 'Mark Addy', rating: 4.8, age: 55, gender: 'Male', city: 'Tang Hall', picture: '../../../assets/pictures/mark-addy.jpeg', list: 525 }
+    ]
+  },
+  {
+    id: 845, name: 'Added to group hired', order: 4, disabled: true, color: '#F9F9F9', applicants: [
+      { name: 'Michelle Fairley', rating: 4.8, age: 55, gender: 'Female', city: 'Coleraine', picture: '../../../assets/pictures/michelle-fairley.jpeg', list: 845 },
+      { name: 'Nikolaj Coster-Waldau', rating: 4.7, age: 48, gender: 'Male', city: 'Rudkøbing', picture: '../../../assets/pictures/nikolaj-coster-waldau.jpeg', list: 845 },
+      { name: 'Richard Madden', rating: 4.2, age: 32, gender: 'Male', city: 'Elderslie', picture: '../../../assets/pictures/richard-madden.jpeg', list: 845 },
+      { name: 'Sean Bean', rating: 5, age: 59, gender: 'Male', city: ' Handsworth', picture: '../../../assets/pictures/sean-bean.jpeg', list: 845 }
+    ]
+  }
 ];
--- src/app/models/list.ts
+++ src/app/models/list.ts
@@ -1,6 +1,10 @@
+import { Applicant } from './applicant';
+
 export class List {
     id: number;
     name: string;
     order: number;
     disabled: boolean;
+    color: string;
+    applicants: Applicant[];
 }
