a4ae6d20aa26db822c81e76a8ffcb20eb5548652
Jan Campens <jan.campens@gmail.com>
2019-03-22 13:01:13 +0100

    Replace mock with service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-
-import { LISTS } from './mock/mock-lists';
+import { Component, OnInit } from '@angular/core';
+import { ListService } from './services/list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  lists = LISTS;
+export class AppComponent implements OnInit {
+  lists;
+
+  constructor(
+    private listService: ListService
+  ) { }
+
+  ngOnInit() {
+    this.listService.getLists()
+      .subscribe(lists => {
+        this.lists = lists;
+      });
+  }
 }
