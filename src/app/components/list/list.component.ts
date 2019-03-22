15627c35166e8939a5f672b9711b902be302419a
Jan Campens <jan.campens@gmail.com>
2019-03-22 13:01:29 +0100

    Call API when moving cards

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
+import { ApplicantService } from '../../services/applicant.service';
+import { List } from 'src/app/models/list';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  @Input() list;
-  constructor() { }
+  @Input() list: List;
+  @Input() id: number;
+
+  constructor(
+    private applicantService: ApplicantService
+  ) { }
 
   ngOnInit() {
   }
@@ -17,6 +24,8 @@ export class ListComponent implements OnInit {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
+      this.applicantService.updateApplicant(event.previousContainer.data[event.previousIndex], event.container.id, event.currentIndex)
+        .subscribe();
       transferArrayItem(event.previousContainer.data,
         event.container.data,
         event.previousIndex,
